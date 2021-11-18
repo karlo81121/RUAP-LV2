b96721adb30f2f75dee6ecbbb341754d847ad179
karlo81121 <adzic.karlo74@gmail.com>
2021-11-19 00:28:40 +0100

    exercise 3 task 2

--- RUAP-LV2/Controllers/ContactController.cs
+++ RUAP-LV2/Controllers/ContactController.cs
@@ -22,5 +22,14 @@ namespace RUAP_LV2.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
