b77e72b45a5a7abb49c7c220bf24514e50c702b4
karlo81121 <adzic.karlo74@gmail.com>
2021-11-19 00:01:15 +0100

    task 3

--- RUAP-LV2/Controllers/ContactController.cs
+++ RUAP-LV2/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RUAP_LV2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,12 +10,20 @@ namespace RUAP_LV2.Controllers
 {
     public class ContactController : ApiController
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
+            return new Contact[]
             {
-        "Hello",
-        "World"
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
             };
         }
     }
